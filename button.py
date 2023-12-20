7a7c30bbd417891b5bca12147bd82e1029cb33b5
Катя <user3737373737@github.ru>
2023-12-21 01:59:56 +0300

    LДобавление файла requirementsâ

--- button.py
+++ button.py
@@ -7,11 +7,3 @@ class Button:
         self.text = text
         self.pos = pos
         self.size = size
-"""
-    def draw(self, img, color):
-        x, y = self.pos
-        w, h = self.size
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.rectangle(img, self.pos, (x + w, y + h), color, cv2.FILLED)
-        cv2.putText(img, self.text, (x + 8, y + 47), font, 2, (255, 255, 255), 2, cv2.LINE_AA)
-"""
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
 final_txt = ''
 
+
 BLUE = (255, 255, 0)
 GREEN = (0, 255, 0)
 flag = BLUE
